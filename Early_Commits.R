45266fb2f9331a925c0164d249b512f9d2b27db9
wolvesdeejay <westmids1987@googlemail.com>
2024-05-30 03:32:47 +0100

    The first few commits in this repository

---
+++ Early_Commits.R
@@ -0,0 +1,41 @@
+Microsoft Windows [Version 10.0.19045.4170]
+(c) Microsoft Corporation. All rights reserved.
+
+C:\xampp\htdocs\dashboard\websites\OFFICIAL\zahran_unoffical_projects>git status
+On branch master
+Your branch is up to date with 'origin/master'.
+
+Changes to be committed:
+  (use "git restore --staged <file>..." to unstage)
+        new file:   README.md
+
+
+C:\xampp\htdocs\dashboard\websites\OFFICIAL\zahran_unoffical_projects>git commit -m ""
+Aborting commit due to empty commit message.
+
+C:\xampp\htdocs\dashboard\websites\OFFICIAL\zahran_unoffical_projects>git status
+On branch master
+Your branch is up to date with 'origin/master'.
+
+Changes to be committed:
+  (use "git restore --staged <file>..." to unstage)
+        new file:   README.md
+
+
+C:\xampp\htdocs\dashboard\websites\OFFICIAL\zahran_unoffical_projects>git commit -m "README.md"
+[master 193c863] README.md
+ 1 file changed, 1 insertion(+)
+ create mode 100644 README.md
+
+C:\xampp\htdocs\dashboard\websites\OFFICIAL\zahran_unoffical_projects>git push
+Enumerating objects: 4, done.
+Counting objects: 100% (4/4), done.
+Delta compression using up to 4 threads
+Compressing objects: 100% (2/2), done.
+Writing objects: 100% (3/3), 316 bytes | 316.00 KiB/s, done.
+Total 3 (delta 1), reused 0 (delta 0), pack-reused 0
+remote: Resolving deltas: 100% (1/1), completed with 1 local object.
+To https://github.com/wolvesdeejay/zahran_unofficial_projects
+   e086bdf..193c863  master -> master
+
+C:\xampp\htdocs\dashboard\websites\OFFICIAL\zahran_unoffical_projects>
